543c21c9ce5a4d698a8b55357623f7fd4fe7b02a
Rebecca Turk <rebeccaturk@Rebeccas-MacBook-Pro-2.local>
2018-06-22 11:00:41 -0400

    drawStairs RTURK

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -12,10 +12,28 @@
 ******
 
 */
-
+console.log('=============RUNNING PROGRAM========')
 function drawStairs(n) {
-
+    // for loop
+    // add n *'s to row (innerStr)
+    // add n rows to overall return (outerStr)
+    // '/n' operator to break up lines when concatenating innerStr into outerStr? 
+    // now I have a right to left staircase...
+    // To make L to R, add a second loop and ...
+    let outerStr = '';
+    let innerStr = '';
+    for (let j = 1; j <= n; j++) {
+        innerStr += '*';
+        console.log('j: ', j);
+        console.log('innerStr: ', innerStr);
+        console.log('outerStr: ', outerStr);
+        outerStr += innerStr;
+        outerStr += '\n';
+    }
+    console.log('outerStr += innerStr: ', outerStr)
+    return outerStr;
 }
 
+console.log(drawStairs(10));
 
 module.exports = drawStairs;
