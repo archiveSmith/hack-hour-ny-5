6b2e6b87e05449a223c06cd34035db9c539c3992
Rebecca Turk <rebeccaturk@Rebeccas-MacBook-Pro-2.local>
2018-07-02 08:37:18 -0400

    completed string-rotation RTURK

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -15,8 +15,27 @@ function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
-function stringRotation(s1, s2) {
+function stringRotation(s1, s2, numOfCharsRotating) { // it seems this needs another parameter for how many characters are being rotated.
+  // critera to be true: 
+  // 1) Must be same length (length property) X
+  // 2) must be a true rotation and not anagram. Chunks of the string, at least 2 consecutive chars, must be in order 
 
+  let isRotation = false;
+
+  if (s2.length < s1.length) { // if second string is less than the len of the primary string => false
+    return isRotation;
+  } 
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1.slice(i, numOfCharsRotating) !== s2.slice(numOfCharsRotating)) { // if, for example, in the case of stringRotation("hello", "llohe") -> true, if "he", the chars at indexes 0-2 of s1 === "he" the last 2 chars of s2, then it is a rotation. This seems to be consistent with my understanding of what a rotation is.
+      isRotation = true;;
+    }
+  }
+  return isRotation;
 }
+
+// console.log(stringRotation("hello", "llohe", 2));  // => true
+// console.log(stringRotation("hello", "he")); // => false
+console.log(stringRotation("hello", "hello")); // => true
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
