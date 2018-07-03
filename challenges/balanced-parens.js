66441a83f3cc7e082b2942632c81eb2c9cfba95a
Christopher Wong <cwong8257@gmail.com>
2018-07-03 10:28:42 -0400

    Finished balanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,26 @@
  *
  */
 
-function balancedParens(input){
+function balancedParens(input) {
+  const stack = [];
+  const strippedInput = input.replace(/[^\(\)\[\]\{\}]/, '');
+  const openingParens = '({[';
+  const closingParens = ')}]';
 
+  for (let char of strippedInput) {
+    const lastInStack = stack[stack.length - 1];
+    const matchingOpeningParen = closingParens.indexOf(char);
+
+    if (openingParens.includes(char)) stack.push(char);
+    else if (openingParens[matchingOpeningParen] === lastInStack) stack.pop();
+    else return false;
+  }
+
+  return stack.length === 0;
 }
+
+console.log(balancedParens('[](){}'));
+console.log(balancedParens('[({})]'));
+console.log(balancedParens('[(]{)}'));
 
 module.exports = balancedParens;
