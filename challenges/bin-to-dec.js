31cf7381299a97364cfd4596ee32ea0987ffe572
Christopher Wong <cwong8257@gmail.com>
2018-07-07 19:58:22 -0400

    Finished bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,15 @@
  */
 
 function binToDec(binary) {
+  let sum = 0;
 
+  for (let i = binary.length - 1; i >= 0; i--) {
+    if (binary[i] === '1') {
+      sum += Math.pow(2, binary.length - i - 1);
+    }
+  }
+
+  return sum;
 }
 
 module.exports = binToDec;
