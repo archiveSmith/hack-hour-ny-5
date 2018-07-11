c784e16586317a3b756c19b742d1950d6c399847
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-07-11 09:48:00 -0400

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,28 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length >= 3) {
+    const firstThree = array.slice(0,3).sort();
+    let largestNumber = firstThree[2];
+    let secondLargestNumber = firstThree[1];
+    let thirdLargestNumber = firstThree[0]; 
+    for (let i = 3; i < array.length; i++) {
+      console.log(largestNumber + ', ' + secondLargestNumber + ', ' + thirdLargestNumber);
+      if (array[i] > largestNumber) {
+        thirdLargestNumber = secondLargestNumber;
+        secondLargestNumber = largestNumber;
+        largestNumber = array[i];
+      }
+      else if (array[i] > secondLargestNumber) {
+        thirdLargestNumber = secondLargestNumber;
+        secondLargestNumber = array[i];
+      }
+      else if (array[i] > thirdLargestNumber) {
+        thirdLargestNumber = array[i];
+      }
+    }
+    return largestNumber * secondLargestNumber * thirdLargestNumber;
+  }
 }
-
 
 module.exports = highestProduct;
