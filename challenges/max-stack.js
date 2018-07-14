06a7361686926b4a76ec50bb1eeca0b6ad0572ea
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-07-14 09:41:35 -0400

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,28 @@
  */
 
 function Stack() {
-  // body...
+  this.arr = [];
+}
+
+Stack.prototype.push = function (value) {
+  if (! this.arr) {
+    this.arr.push(value);
+  }
+  let ind = this.arr.length;
+  while (ind >= 1 && this.arr[ind - 1] > value) {
+    this.arr[ind] = this.arr[ind -1];
+    ind--;
+  }
+  this.arr[ind] = value;
+  return this.arr.length;
+}
+
+Stack.prototype.getMax = function() {
+  return this.arr[this.arr.length - 1];
+}
+
+Stack.prototype.pop = function() {
+  return this.arr.pop();
 }
 
 module.exports = Stack;
