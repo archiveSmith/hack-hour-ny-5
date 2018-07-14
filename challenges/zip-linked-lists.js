a5c907b21222c6d5a2128df3122743178447362d
Christopher Wong <cwong8257@gmail.com>
2018-07-14 10:15:51 -0400

    Finished max-stack

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,19 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  let head1 = l1;
+  let head2 = l2;
+  let next1 = head1.next;
+  let next2 = head2.next;
 
-module.exports = {Node: Node, zip: zip};
+  while (head1 && head2) {
+    head1.next = head2;
+    head1 = next1;
+    next1 = next1.next;
+    head2.next = head1;
+    head2 = next2;
+    next2 = next2.next;
+  }
+}
+
+module.exports = { Node: Node, zip: zip };
