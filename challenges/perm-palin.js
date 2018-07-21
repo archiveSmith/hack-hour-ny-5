6601193fcebdcdf81d5f60fed7ac57a5f7f6eab1
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-07-21 10:17:23 -0400

    perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,24 @@
  */
 
 function permPalin(str) {
-	
+    let numOdd = 0;
+    const countObj = {};
+    for (let i = 0; i < str.length; i++) {
+        const curChar = str.charAt(i);
+        if (countObj[curChar]) {
+            countObj[curChar]++;
+        }
+        else {
+            countObj[curChar] = 1;
+        }
+        if (countObj[curChar] % 2 === 1) {
+            numOdd++;
+        }
+        else {
+            numOdd--;
+        }
+    }
+    return numOdd <= 1;
 }
 
 module.exports = permPalin;
