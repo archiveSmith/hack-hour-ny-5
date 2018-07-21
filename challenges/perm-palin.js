3dfd220735501335b454b8e198ae1eb4c4349fc8
Christopher Wong <cwong8257@gmail.com>
2018-07-21 10:23:32 -0400

    Finished perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,15 @@
  */
 
 function permPalin(str) {
-	
+  const table = str.split('').reduce((a, char) => ((a[char] = !a[char]), a), {});
+  const count = Object.values(table).reduce((a, value) => (value ? a + 1 : a), 0);
+
+  return count <= 1;
 }
+
+console.log(permPalin('abab'));
+console.log(permPalin('cbaba'));
+console.log(permPalin('cbac'));
+console.log(permPalin('a'));
 
 module.exports = permPalin;
