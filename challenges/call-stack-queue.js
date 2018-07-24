5c36e85b2bf3e4345d38e020353bf90ce07f2855
Rebecca Turk <rebeccaturk@Rebeccas-MacBook-Pro-2.local>
2018-07-24 10:04:34 -0400

    made my own file because git was down

---
+++ challenges/call-stack-queue.js
@@ -0,0 +1,21 @@
+/**
+ * Create a stack.Then create a queue using two stacks.
+ */
+
+
+function Stack() {
+    this.storage = {};
+    this.index = 0;
+}
+
+
+/**
+* Queue Class
+*/
+
+
+function Queue() {
+    let firstStack = new Stack();
+}
+
+module.exports = {Stack: Stack, Queue: Queue};
