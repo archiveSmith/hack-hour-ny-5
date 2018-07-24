285ae57bddec5662b0561e0675530279bb7eba30
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-07-24 09:53:52 -0400

    challenges/stack-queue.js

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,21 @@
 
 
 function Stack() {
+    this.storage = [];
+    this.length = 0;
+}
 
+Stack.prototype.push = function(value) {
+    this.storage.push(value);
+    this.length++;
+}
+
+Stack.prototype.pop = function() {
+    if (this.length > 0) {
+        const top = this.storage[--this.length];
+        delete this.storage[this.length];
+        return top;
+    }
 }
 
 
@@ -14,7 +28,23 @@ function Stack() {
 
 
 function Queue() {
+    this.pushStack = new Stack();
+    this.popStack = new Stack();
+}
 
+Queue.prototype.push = function(value) {
+    this.pushStack.push(value);
+}
+
+Queue.prototype.pop = function() {
+    if (this.pushStack.length + this.popStack.length > 0) {
+        if (this.popStack.length === 0) {
+            while (this.pushStack.length > 0) {
+                this.popStack.push(this.pushStack.pop());
+            }
+        }
+        return this.popStack.pop();
+    }
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
