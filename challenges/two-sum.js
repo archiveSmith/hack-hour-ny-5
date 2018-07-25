714728466e711153dabcf8f329e1f52b6f8fa277
Julia Ieshtokina <julia.ieshtokina@gmail.com>
2018-07-25 10:24:28 -0400

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,19 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+ffunction twoSum(arr, n) {
+    let length = arr.length;
+      for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+          if(i !== j) {
+            if (arr[i] + arr[j] === n) return true;
+          }
+        }
+      }
+    return false;
+  }
+  
+  console.log(twoSum([1,2,3], 6));
 
-}
 
 module.exports = twoSum;
