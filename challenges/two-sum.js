db4fe7b67839db30775d2de372fae27ac907552e
Christopher Wong <cwong8257@gmail.com>
2018-07-25 10:13:37 -0400

    Finished two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,19 @@
  */
 
 function twoSum(arr, n) {
+  const lookup = {};
+  const { length } = arr;
 
+  for (let i = 0; i < length; i++) {
+    const num = arr[i];
+    lookup[num] = true;
+    const difference = target - num;
+    if (lookup[difference]) return true;
+  }
+
+  return false;
 }
+
+console.log(twoSum([1, 2, 3, 4, 5], 7));
 
 module.exports = twoSum;
