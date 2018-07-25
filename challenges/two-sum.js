333543d859ede20bf1fefc4d708daffbc00f3308
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-07-25 10:58:20 -0400

    two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,15 @@
  */
 
 function twoSum(arr, n) {
-
+    const seenNumbers = {};
+    for (let i = 0; i < arr.length; i++) {
+        const cur_num = arr[i];
+        if (seenNumbers[n - cur_num]) {
+            return true;
+        }
+        seenNumbers[cur_num] = true;
+    }
+    return false;
 }
 
 module.exports = twoSum;
