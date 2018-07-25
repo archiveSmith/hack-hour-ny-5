045db33a7fd1ccb39d5f974214a3372d84619d4f
budken <siu.afmm@gmail.com>
2018-07-25 09:42:29 -0400

    Yolo

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -2,19 +2,25 @@
  * Create a stack.Then create a queue using two stacks.
  */
 
-
 function Stack() {
-
+  this.body = [];
 }
 
+Stack.prototype.add = function(value) {
+  this.body.push(value);
+};
+
+Stack.prototype.remove = function() {
+  let removed = this.body.pop();
+  return removed;
+};
 
 /**
-* Queue Class
-*/
-
-
+ * Queue Class
+ */
+// Push everything into stack1 then pop each element to stack2. stack2 is now in reversed order. Pop out to be evaluated
 function Queue() {
-
+  this.queue = 
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+module.exports = { Stack: Stack, Queue: Queue };
