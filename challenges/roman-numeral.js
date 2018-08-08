731de02c33979fc98afd358c092ed31a07a5fd60
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-08-08 10:09:20 -0400

    roman numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,37 @@
  */
 
 function romanNumeral(n) {
+    function convertToRoman(number) {
+        const numbers = [1000, 500, 100, 50, 10, 5, 1];
+        const letters = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+        let pos = 0;
+        let left = number;
+        let str = '';
+        while (left > 0) {
+            if (left >= numbers[pos]) {
+                str += letters[pos];
+                left -= numbers[pos];
+            }
+            else if (pos % 2 === 0 && left >= (numbers[pos] - 2 * numbers[pos + 2])) {
+                return convertToRoman(numbers[pos] - left) + letters[pos];
+            }
+            else if (pos % 2 === 1 && left >= (4 * numbers[pos+1])) {
+                return convertToRoman(numbers[pos] - left) + letters[pos];
+            }
+            else {
+                pos++;
+            }
+        }
+        return str;
+    }
 
+    let romanString = '';
+    let numStr = String(n);
+    for (let i = 0; i < numStr.length; i++) {
+        let curDigit = Number(numStr.charAt(numStr.length - i - 1));
+        romanString = convertToRoman(curDigit * Math.pow(10, i)) + romanString; 
+    }
+    return romanString;
 }
 
 module.exports = romanNumeral;
