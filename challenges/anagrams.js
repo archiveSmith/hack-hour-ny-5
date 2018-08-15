81d29da1e5df83eb5aa1f3e8b96a7a1782a39cc0
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-08-15 09:57:46 -0400

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,30 @@
   */
 
 function anagrams(string) {
-
+  if (string.length === 0) {
+    return [];
+  }
+  if (string.length === 1) {
+    return [string];
+  }
+  const result = [];
+  const rest = anagrams(string.substring(1));
+  rest.forEach(str => {
+    for (let i = 0; i <= str.length; i++) {
+      const curString = str.substring(0,i) + string.charAt(0) + str.substring(i);
+      let newString = true;
+      for (let j = 0; j < result.length; j++) {
+        if (result[j] === curString) {
+          newString = false;
+          break;
+        }
+      }
+      if (newString) {
+        result.push(curString);
+      }
+    }
+  });
+  return result;
 }
 
 module.exports = anagrams;
