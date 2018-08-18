414d108e80744f5cd91b778e2e9566420707ed11
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-08-18 09:53:59 -0400

    find in ordered

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,7 +11,21 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+    let min = 0;
+    let max = arr.length-1;
+    while (min <= max) {
+        const mid = (min + max) / 2;
+        if (arr[mid] === target) {
+            return true;
+        }
+        else if (arr[mid] < target) {
+            min = mid + 1;
+        }
+        else {
+            max = mid - 1;
+        }
+    }
+    return false;
 }
 
 
