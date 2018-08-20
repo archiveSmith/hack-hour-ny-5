687e39271013b71aa186e29be1154225c0ad413e
Manuel Castro <manuel.a.castro1992@gmail.com>
2018-08-20 10:31:21 -0400

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,18 @@
 
 
 function insertionSort(array) {
-
+    for (let i = 1; i < array.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (array[i] < array[j]) {
+                const newEle = array[i];
+                for (let k = i; k > j; k--) {
+                    array[k] = array[k-1];
+                }
+                array[j] = newEle;
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = insertionSort;
