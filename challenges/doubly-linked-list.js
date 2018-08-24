876b8217b6c49de716ecd3fbf08d37618cc3b388
Julia Ieshtokina <julia.ieshtokina@gmail.com>
2018-08-24 18:23:46 -0400

    doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,31 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if(this.head === null) {
+    this.head = new Node(val);
+    this.tail = this.head;
+  } else {
+    this.tail.next = new Node(val);
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if(this.head.val === val) {
+    this.head = this.head.next;
+  } else {
+    let curr = this.head;
+    while(curr.val !== val && curr) {
+      // console.log('current node', curr);
+      curr = curr.next;
+    } 
+    curr.prev.next = curr.next;
+    curr.next.prev = curr.prev;
+  }  
 };
 
 module.exports = LinkedList;
