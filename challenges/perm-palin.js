b821c824a70878941909f6188a39d4d4eb173a7e
Christopher Wong <cwong8257@gmail.com>
2018-10-29 10:39:00 -0400

    Refactor knapsack-problem and perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,12 +9,41 @@
  *
  */
 
+// function permPalin(str) {
+//   const stack = [];
+//   const charArray = str.split('').sort();
+
+//   for (let char of charArray) {
+//     if (stack[stack.length - 1] !== char) {
+//       stack.push(char);
+//     } else {
+//       stack.pop();
+//     }
+//   }
+
+//   return stack.length <= 1;
+// }
+
 function permPalin(str) {
-  const table = str.split('').reduce((a, char) => ((a[char] = !a[char]), a), {});
-  const count = Object.values(table).reduce((a, value) => (value ? a + 1 : a), 0);
+  const table = {};
+  let count = 0;
+
+  for (let char of str) {
+    table[char] = !table[char];
+  }
+
+  for (let char in table) {
+    if (table[char] && ++count > 1) {
+      return false;
+    }
+  }
 
   return count <= 1;
 }
+
+// function permPalin(str) {
+//   return Object.values(str.split('').reduce((a, char) => ((a[char] = !a[char]), a), {})).reduce((a, value) => (value ? a + 1 : a), 0) <= 1;
+// }
 
 console.log(permPalin('abab'));
 console.log(permPalin('cbaba'));
