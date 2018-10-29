e8c428a5fd53b6548c2b0863b28605c90b8fa19d
Christopher Wong <cwong8257@gmail.com>
2018-10-29 14:06:24 -0400

    Refactor rotate-grid

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -16,20 +16,50 @@
  * BONUS: Do this in place
  */
 
-function rotateGrid(grid, n) {
-  const newGrid = [[], [], []];
+// function rotateGrid(grid, n) {
+//   const newGrid = [];
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      newGrid[j][n - i - 1] = grid[i][j];
+//   for (let col = 0; col < n; col++) {
+//     const newRow = [];
+
+//     for (let row = n - 1; row >= 0; row--) {
+//       newRow.push(grid[row][col]);
+//     }
+
+//     newGrid.push(newRow);
+//   }
+
+// }
+
+function rotateGrid(grid, n) {
+  for (let x = 0; x < Math.ceil(n / 2); x++) {
+    for (let y = x; y < n - x - 1; y++) {
+      let save;
+      let transfer = grid[x][y];
+
+      for (let k = 0; k < 4; k++) {
+        save = grid[y][n - x - 1];
+        grid[y][n - x - 1] = transfer;
+        transfer = save;
+        let temp = x;
+        x = y;
+        y = n - temp - 1;
+      }
     }
   }
-
-  return newGrid;
 }
 
-const sampleGrid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-console.log(rotateGrid(sampleGrid, 3));
+
+const sampleGrid2 = [[1, 2], [3, 4]];
+const sampleGrid3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const sampleGrid4 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+
+rotateGrid(sampleGrid2, 2)
+console.log(sampleGrid2);
+rotateGrid(sampleGrid3, 3);
+console.log(sampleGrid3);
+rotateGrid(sampleGrid4, 4);
+console.log(sampleGrid4);
 
 module.exports = rotateGrid;
